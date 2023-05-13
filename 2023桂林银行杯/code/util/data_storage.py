d0c5502d21a45d4063d4e14ee92c8b37a69c0c09
codezzzsleep <codezzzsleep@163.com>
2023-05-13 19:48:15 +0800

    add file

--- 2023桂林银行杯/code/util/data_storage.py
+++ 2023桂林银行杯/code/util/data_storage.py
@@ -1,5 +1,9 @@
 import os
 
-# def train_make_dir():
-tmp = os.listdir("../run/detect")
-print(tmp)
+
+def train_make_dir():
+    tmp = os.listdir("../run/train")
+    return int(tmp[len(tmp)-1])+1
+def detect_make_dir():
+    tmp = os.listdir("../run/detect")
+    return int(tmp[len(tmp) - 1]) + 1
