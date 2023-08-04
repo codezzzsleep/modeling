78cc24906d1447da59b9c192b29a7ef1bf1bf85d
codezzzsleep <codezzzsleep@163.com>
2023-08-04 17:28:32 +0800

    add

--- 华数杯/code/loader.py
+++ 华数杯/code/loader.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 df = pd.read_excel("../dataset/demo.xlsx")
+df = df.drop(0)
 df = df.iloc[:, :15]
-print(df)
+outliers = df[(df['母亲年龄'] < 16) | (df['母亲年龄'] > 75)]
+print(outliers)
