438db0da33ce4b1496a98256044de719e14b8b0d
codezzzsleep <codezzzsleep@163.com>
2023-09-04 01:42:46 +0800

    add

--- 资料/传统机器学习/神经网络/main.py
+++ 资料/传统机器学习/神经网络/main.py
@@ -4,36 +4,64 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import RMSprop
 from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, roc_auc_score, auc
+from sklearn.preprocessing import label_binarize
 
-# 加载 MNIST 数据集
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# 将数据折叠到1维结构，即 (num_samples, 784)
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
 
-# 标准化输入数据（0-255 的整数值转换为 0-1 的浮点值）
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 
-# 将类别向量（类别标签）转换为二进制类矩阵（即 one-hot 编码）
 y_train = to_categorical(y_train, 10)
 y_test = to_categorical(y_test, 10)
 
-# 创建一个顺序模型（sequential model），然后添加层次
 model = Sequential()
 model.add(Dense(512, activation='relu', input_shape=(784,)))
 model.add(Dense(256, activation='relu'))
 model.add(Dense(128, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
-# 编译模型
 model.compile(loss='categorical_crossentropy', optimizer=RMSprop(), metrics=['accuracy'])
 
-# 拟合（训练）模型训练集；设置批处理大小和迭代轮数
 model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
 
-# 评估模型
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+y_pred = model.predict(x_test)
+y_pred_classes = y_pred.argmax(axis=1)
+y_true_classes = y_test.argmax(axis=1)
+
+cm = confusion_matrix(y_true_classes, y_pred_classes)
+
+plt.figure(figsize=(10, 10))
+sns.heatmap(cm, annot=True, fmt="d", cmap="YlGnBu")
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+plt.savefig("logs/BP_heatmap.png")
+plt.show()
+
+report = classification_report(y_true_classes, y_pred_classes)
+print(report)
+
+y_test_binarized = label_binarize(y_true_classes, classes=range(10))
+y_pred_binarized = label_binarize(y_pred_classes, classes=range(10))
+
+for i in range(10):
+    fpr, tpr, thresholds = roc_curve(y_test_binarized[:, i], y_pred_binarized[:, i])
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, lw=2, label=f'Class {i} (AUC = {roc_auc:.2f})')
+
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC curves for each class')
+plt.legend(loc="lower right")
+plt.savefig("logs/BP_ROC.png")
+plt.show()
